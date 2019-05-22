0d7340c39189803bc722dcdbc060538017c8a093
Chris Carbajal <chriscarbajal@chriss-macbook-pro.local>
2019-05-21 18:02:35 -0700

    Initial Commit

---
+++ Play Out/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Play Out
+//
+//  Created by Chris Carbajal on 5/21/19.
+//  Copyright © 2019 Chris Carbajal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Play OutTests/Play_OutTests.swift
@@ -0,0 +1,34 @@
+//
+//  Play_OutTests.swift
+//  Play OutTests
+//
+//  Created by Chris Carbajal on 5/21/19.
+//  Copyright © 2019 Chris Carbajal. All rights reserved.
+//
+
+import XCTest
+@testable import Play_Out
+
+class Play_OutTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
